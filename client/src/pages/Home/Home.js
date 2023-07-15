6d88d9c8675f8cd3f971b066b5963b8814db3783
Paweł <fordcrown2009@gmail.com>
2023-07-15 04:20:12 +0200

    Particles in home

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -12,6 +12,9 @@ import AccessibilityIcon from '@mui/icons-material/Accessibility';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 
+import Particles from 'react-tsparticles'
+import { loadFull } from "tsparticles";
+
 import { ThreeDots, TailSpin } from 'react-loading-icons'
 
 import { useSearchParams } from "react-router-dom";
@@ -61,13 +64,104 @@ const Home = () => {
 
     setTimeout(() => {
       setShowSpinner(false);
-    }, 150);
+    }, 50);
   }
 
   checkInformations(key);
 
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
   return (
     <div className="home">
+      <Particles
+        className="particles"
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          background: {
+            color: 'transparent',
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: 'canvas',
+            events: {
+              resize: false
+            },
+          },
+          "particles": {
+            "number": {
+              "value": 40,
+              "density": {
+                "enable": true,
+                "value_area": 400
+              }
+            },
+            "color": {
+              "value": "#ffffff"
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              "polygon": {
+                "nb_sides": 5
+              },
+              "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+              }
+            },
+            "opacity": {
+              "value": 0.5,
+              "random": false,
+              "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 1,
+              "random": true,
+              "anim": {
+                "enable": false,
+                "speed": 10,
+                "size_min": 0.1,
+                "sync": false
+              }
+            },
+            "line_linked": {
+              "enable": true,
+              "distance": 200,
+              "color": "#ffffff",
+              "opacity": 0.05,
+              "width": 1
+            },
+            "move": {
+              "enable": true,
+              "speed": 1,
+              "direction": "none",
+              "random": false,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+              }
+            }
+          },
+          "retina_detect": true
+        }}
+      />  
+
       <Spinner open={showSpinner} />
       
       <div className="sidebar">
@@ -77,7 +171,7 @@ const Home = () => {
               style={{
                 position: "absolute",
                 top: "47px",
-                left: "100px",
+                left: "90px",
                 width: "20px",
                 margin: "0"
               }}
@@ -87,7 +181,7 @@ const Home = () => {
               style={{
                 position: "absolute",
                 top: "70px",
-                left: "165px",
+                left: "155px",
                 width: "15px",
                 margin: "0",
               }}
